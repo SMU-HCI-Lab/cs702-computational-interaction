2fb76409357d7b092846c45192b7ff44fa8a22e4
Kotaro <koe.bluebear@gmail.com>
2024-01-08 20:56:40 +0800

    Change file names

---
+++ week-01/exercise/01-getting-started.py
@@ -0,0 +1,17 @@
+"""
+Exercise 1.1: Getting Started
+"""
+
+
+if __name__ == "__main__":
+    # 1. Ask a question by printing "Guess what I ate today for lunch!" to the standard output.
+
+    # 2. Provide options A, B, and C. (e.g., A: Pizza, B: Burger, C: Salad) by printing them to the standard output.
+
+    # 3. In a loop, prompt the user to select option A, B, or C.
+    # 3.1. If the user input is correct, print "You are right!" to the standard output and break the loop.
+    # 3.2. If the user input is incorrect, print "Try again!" to the standard output.
+
+
+    # 4. Play the game with your partners!
+    pass
