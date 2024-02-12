f21dc6fd6eff8f6f646efa3f67cf43ef9adfad4b
Kotaro <koe.bluebear@gmail.com>
2024-02-12 19:45:33 +0800

    Scroll control

--- week-06/lecture/scroll.py
+++ week-06/lecture/scroll.py
@@ -8,9 +8,8 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Scroll Simulation")
 
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (200, 200, 200)
+DARK_RED = (100, 100, 100)
 BLACK = (0, 0, 0)
 
 a, b, c = 1, 0.5, 0
@@ -24,6 +23,9 @@ u = np.zeros_like(t)
 u[10:20] = 3
 u[50:60] = -2
 u[80:90] = 5
+u[100:110] = -3
+u[150:160] = 4
+u[200:210] = -2
 t, y, x = signal.lsim(SS, u, t)
 
 position_scale = 50
@@ -44,10 +46,31 @@ while running:
     if current_time < len(t):
         position = x[current_time, 0] * position_scale
 
-        display_height = 100
+        rect_height = 100
         padding = 10
         # Plot a rectangle with stroke and no fill
-        pygame.draw.rect(screen, WHITE, (padding, padding + position, width - 2 * padding, display_height))
+        pygame.draw.rect(screen,
+                         WHITE,
+                         (padding, padding + position, width - 2 * padding, rect_height))
+
+    # Draw a circle to indicate the input
+    input_x_origin = width - 50
+    input_y_origin = 500
+    scale = 10
+    input_position = input_y_origin + u[current_time]
+    pygame.draw.line(screen,
+                     RED,
+                     (input_x_origin, input_y_origin),
+                     (input_x_origin, int(u[current_time] * scale + input_y_origin)),
+                     1)
+    pygame.draw.circle(screen,
+                       DARK_RED,
+                       (input_x_origin, input_y_origin),
+                       7)
+    pygame.draw.circle(screen,
+                       RED,
+                       (input_x_origin, int(u[current_time] * scale + input_y_origin)),
+                       10)
 
     pygame.display.flip()
     clock.tick(60)
